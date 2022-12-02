eeeaec4a9b63c9a55a1b6fe44712f25c379afd74
kfifel <fifle.khalid99@gmail.com>
2022-12-02 20:13:01 +0100

    add attribut of class patient et person

--- classes/Patient.php
+++ classes/Patient.php
@@ -1,5 +1,25 @@
 <?php
 
-class Patient
+use Cassandra\Date;
+
+class Patient extends Person
 {
+    public date $date_of_birth;
+
+    public function __construct(int $id, string $first_name, string $last_name, string $email, string $password, string $role,date $date_of_birth)
+    {
+        $this->date_of_birth = $date_of_birth;
+        parent::__construct($id, $first_name, $last_name, $email, $password, $role);
+    }
+
+    function deleteMyAccount()
+    {
+        // TODO implement here
+    }
+
+    public function takeAppointment()
+    {
+        // TODO implement here
+    }
+
 }
--- classes/Person.php
+++ classes/Person.php
@@ -2,4 +2,22 @@
 
 abstract class Person
 {
+
+    protected int $id;
+    protected string $first_name;
+    protected string $last_name;
+    protected string $email;
+    protected string $password;
+    protected string $role;
+
+    public function __construct(int $id, string $first_name, string $last_name, string $email, string $password, string $role)
+    {
+        $this->id = $id;
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+        $this->email = $email;
+        $this->password = $password;
+        $this->role = $role;
+    }
+
 }
