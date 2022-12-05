60510f5c3ce9455296535f72357b60c308a94c08
MohamedJamh <jam7.mohamed@gamil.com>
2022-12-05 11:06:50 +0100

    reforme

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+        $GLOBALS['page_title'] = 'test';
+        include('includes/head.php');
+    ?>
+<body>
+    <?php
+        $GLOBALS['current_page'] = 'dashboard';
+        include('includes/doctor-sidebar.php')
+    ?>
+</body>
+</html>
