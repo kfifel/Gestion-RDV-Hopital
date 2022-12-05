2493f74744579d9a8350fda160ebbd5fd1f03ff2
MohamedJamh <jam7.mohamed@gamil.com>
2022-12-05 12:21:25 +0100

    adding url to sidelinks href

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <?php 
+        $GLOBALS['page_title'] = 'test 2';
+        include('includes/head.php');
+    ?>
+<body>
+    <?php
+        $GLOBALS['current_page'] = 'my_appointment';
+        include('includes/paitent-sidebar.php')
+    ?>
+</body>
+</html>
