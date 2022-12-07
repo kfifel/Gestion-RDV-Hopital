3583022691972f0545259883675afb1e7c44d040
MohamedJamh <jam7.mohamed@gamil.com>
2022-12-07 14:41:22 +0100

    giving up on flowbite, linking admin-dashboard , doctor-dashboard , patient-home with other pages

--- admin-interfaces/admin-dashboard.php
+++ admin-interfaces/admin-dashboard.php
@@ -14,13 +14,13 @@
     <main class="ml-64 p-6 admin-dashboard flex flex-col gap-3">
         <div class="header flex justify-between">
             <h1 class="hidden">Admin Dashboard</h1>
-            <from class="search flex gap-2 items-center w-3/4">
+            <form class="search flex gap-2 items-center w-3/4" method="GET">
                 <div class="border-2 rounded flex items-center h-8 w-2/3">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#616161"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
-                    <input class="p-2 h-7 w-full focus:outline-0" type="text" placeholder="Search doctor name or email">
+                    <input name="search" class="border-0 p-2 h-7 w-full focus:outline-0" type="text" placeholder="Search doctor name or email">
                 </div>
-                <button class="bg-sky-200 px-4 h-8 rounded text-sky-900" >Search</button>
-            </from>
+                <input class="bg-sky-200 px-4 h-8 rounded text-sky-900 pointer" type="submit" value="Search">
+            </form>
             <div class="date flex gap-2">
                 <div class="font-bold">
                     <small class="block text-slate-500 text-right">Today's Date</small>
@@ -90,9 +90,9 @@
                         </tr>
                     </table>
                 </div>
-                <form action="" class="mb-0">
-                    <button class="w-full p-2 bg-sky-700 text-white text-center rounded-md">Show all Appointments</button>
-                </form>
+                <a href="admin-appointment.php">
+                    <div class="w-full p-2 bg-sky-700 text-white text-center rounded-md" >Show all Appointments</div>
+                </a>
             </div>
             <div class="upcoming-sessions block w-full xl:w-1/2">
                 <h2 class="font-bold text-sky-700 text-2xl my-4 xl:text-right">Upcoming Sessions until Next <span>*day*</span></h2>
@@ -111,52 +111,12 @@
                         </tr>
                     </table>
                 </div>
-                <form action="" class="mb-0">
-                    <button class="w-full p-2 bg-sky-700 text-white text-center rounded-md">Show all Sessions</button>
-                </form>
+                <a href="admin-schedule.php">
+                    <div class="w-full p-2 bg-sky-700 text-white text-center rounded-md" >Show all Sessions</div>
+                </a>
             </div>
         </div>
     </main>
-    
-    <!-- Modal toggle -->
-    <button class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="defaultModal">
-    Toggle modal
-    </button>
-
-    <!-- Main modal -->
-    <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-        <div class="relative w-full h-full max-w-2xl md:h-auto">
-            <!-- Modal content -->
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <!-- Modal header -->
-                <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                        Terms of Service
-                    </h3>
-                    <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="defaultModal">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                </div>
-                <!-- Modal body -->
-                <div class="p-6 space-y-6">
-                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.
-                    </p>
-                    <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                        The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to notify users as soon as possible of high-risk data breaches that could personally affect them.
-                    </p>
-                </div>
-                <!-- Modal footer -->
-                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                    <button data-modal-toggle="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I accept</button>
-                    <button data-modal-toggle="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Decline</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
     <?php include('../includes/footer.php');?>
 </body>
 </html>
