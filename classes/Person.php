7911de79927a1fc35c03df0aa8c802686d254e84
hamza harrass <hamzaharrass05@gmail.com>
2022-12-12 11:45:08 +0100

    crees class person

--- classes/Person.php
+++ classes/Person.php
@@ -2,15 +2,14 @@
 
 abstract class Person
 {
+    protected int $id;
+    protected string $first_name;
+    protected string $last_name;
+    protected string $email;
+    protected string $password;
+    protected string $role;
 
-    Protected int|null $id;
-    Protected string $first_name;
-    Protected string $last_name;
-    Protected string $email;
-    Protected string $password;
-    Protected string $role;
-
-    public function __construct(int|null $id, string $first_name, string $last_name, string $email, string $password, string $role)
+    public function __construct(int $id, string $first_name, string $last_name, string $email, string $password, string $role)
     {
         $this->id = $id;
         $this->first_name = $first_name;
